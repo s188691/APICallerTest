e4969d2c71187a58ff1ffca9114ce18feb2793fb
s188691 <wiktor.wnekowicz@gmail.com>
2021-09-23 23:17:02 +0200

    Add project files.

---
+++ APICallerTest/APIDataObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICallerTest
+{
+    class APIData
+    {
+        public string[] APIDataObj { get; set; }
+    }
+    class APIDataObject
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string picture { get; set; }
+    }
+}
+
+//{
+//    "data": [
+//        {
+//        "id": "60d0fe4f5311236168a109ca",
+//            "title": "ms",
+//            "firstName": "Sara",
+//            "lastName": "Andersen",
+//            "picture": "https://randomuser.me/api/portraits/women/58.jpg"
+//        },
+//    ],
+//    "total": 99,
+//    "page": 0,
+//    "limit": 5
+//}
---
+++ APICallerTest/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace APICallerTest
+{
+    public partial class Form1 : Form
+    {
+        private const string m_URL = "https://dummyapi.io/data/v1/";
+        private string m_UrlParameters = "user?page=1&limit=1";
+        private const string m_AppIDKey = "app-id";
+        private const string m_AppIDValue = "614cba9854da1109414ccd6b";
+        private HttpClient m_Client;
+        private APIData m_APIData;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m_Client = new HttpClient();
+            m_Client.BaseAddress = new Uri(m_URL);
+            m_Client.DefaultRequestHeaders.Add(m_AppIDKey, m_AppIDValue);
+
+            if (m_Client.DefaultRequestHeaders.Contains(m_AppIDKey))
+            {
+                HttpResponseMessage response = m_Client.GetAsync(m_UrlParameters).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string dataObject = response.Content.ReadAsStringAsync().Result;
+                    richTextBox1.Text = JsonConvert.DeserializeObject(dataObject).ToString();
+
+
+                    m_APIData = new APIData();
+                    m_APIData.APIDataObj = JsonConvert.DeserializeObject<string[]>(dataObject);
+
+                    richTextBox1.Text = m_APIData.APIDataObj[0];
+
+                    //APIDataObject m_APIOne = new APIDataObject();
+                    //m_APIOne.firstName = m_APIData.APIDataObj[0];
+                    //richTextBox1.Text = m_APIOne.firstName;
+                }
+                else
+                {
+                    richTextBox1.Text =
+                        response.ReasonPhrase.ToString() + " " +
+                        response.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+
+    }
+}
