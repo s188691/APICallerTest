704efa702ed8da38604b782f9572aea97c6c7656
s188691 <wiktor.wnekowicz@gmail.com>
2021-09-24 23:29:55 +0200

    Sensitive data fix

--- APICallerTest/Helper.cs
+++ APICallerTest/Helper.cs
@@ -8,10 +8,6 @@ namespace APICallerTest
 {
     class Helper
     {
-        public const string MAINURL = "https://dummyapi.io/data/v1/";
-        public const string GETURLPARAMS = "user?page=1&limit=1";
-        public const string POSTURLPARAMS = "post/create";
-        public const string APPIDKEY = "app-id";
-        public const string APPIDVALUE = "614cba9854da1109414ccd6b";
+        //Sensitive data :)
     }
 }
